5adac8f8f7d650b75125c68685f09699f1c9f6fb
pmsanchezb <pmsanchezb@gmail.com>
2022-12-31 17:39:53 -0500

    Additional lines

--- cyl.R
+++ cyl.R
@@ -1,7 +1,14 @@
 # Test gitHub
 
 library(tidyverse)
+library(ggplot2)
 
-mtcars %>% 
+data <- mtcars %>% 
   group_by(cyl) %>% 
   summarise(total = sum(hp))
+
+# Additional lines of code
+data %>% 
+  ggplot(aes(x = cyl, y = total)) + 
+  geom_col(fill = "blue") + 
+  theme_bw()
